2f2755edf0dcac8f7826c489d76cde10a6f54f99
abirul-islam-mamun <mamun.billa444@gmail.com>
2023-03-27 12:44:08 +0600

    form css done

--- resources/views/frontend/index/payment.blade.php
+++ resources/views/frontend/index/payment.blade.php
@@ -10,13 +10,15 @@
         </ul>
         <div class="card-wrapper">
             <div class="row">
-                <div class="single-property sale-propety">
-                    <img src="{{asset('storage/post-image/'.$property->image)}}" alt=">
-                    <div class="sale-property-details">
-                        {{-- <span>in</span> {{ $payments->properties->property_id}} --}}
-                        <h5>1450 Cloudcroft Drop</h5>
-                            <p>Illinois / Chicago</p>
-                            <span>$34,000</span>
+                <div class="col-12">
+                    <div class="single-property sale-propety">
+                        {{-- <img src="{{asset('storage/post-image/'.$property->image)}}" alt="> --}}
+                        <div class="sale-property-details">
+                            {{-- <span>in</span> {{ $payments->properties->property_id}} --}}
+                            <h5>1450 Cloudcroft Drop</h5>
+                                <p>Illinois / Chicago</p>
+                                <span>$34,000</span>
+                        </div>
                     </div>
                 </div>
                 <div class="col-12">
@@ -27,35 +29,37 @@
                     @method('POST')
                     @csrf
                     <input type="hidden" name="property_id" value="{{$property->id}}">
-                <div class="col-md-6">
-                    <div class="card-info">
-                        <label for="card_number">Card Number</label>
-                        <input type="text"  name="card_number" placeholder="XXXX - XXXX - XXXX - XXXX">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card-info">
+                                <label for="card_number">Card Number</label>
+                                <input type="text"  name="card_number" placeholder="XXXX - XXXX - XXXX - XXXX">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-info">
+                                <label for="cardholder_number">Card Holder Name</label>
+                                <input type="text" name="cardholder_name" placeholder="Name">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-date">
+                                <label for="card_Name">Expiration Date</label>
+                                <input type="text" name="expired_date" placeholder="Month">
+                                {{-- <input type="text" placeholder="Year"> --}}
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card-date">
+                                <label for="cug_number">Cug_number</label>
+                                <input type="text" name="cug_number" placeholder="000">
+                            </div>
+                        </div>
+                        <div class="more-property mt-20">
+                            <button type="submit"  class="theme-btn">Buy Home</button>
+                            {{-- <a href="{{route('payment.list')}}" class="theme-btn">Buy Home</a> --}}
+                        </div>
                     </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card-info">
-                        <label for="cardholder_number">Card Holder Name</label>
-                        <input type="text" name="cardholder_name" placeholder="Name">
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card-date">
-                        <label for="card_Name">Expiration Date</label>
-                        <input type="text" name="expired_date" placeholder="Month">
-                        {{-- <input type="text" placeholder="Year"> --}}
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="card-date">
-                        <label for="cug_number">Cug_number</label>
-                        <input type="text" name="cug_number" placeholder="000">
-                    </div>
-                </div>
-                <div class="more-property mt-20">
-                    <button type="submit"  class="theme-btn">Buy Home</button>
-                    {{-- <a href="{{route('payment.list')}}" class="theme-btn">Buy Home</a> --}}
-                </div>
                 </form>
                 @else
                     <a href="{{route('login.view')}}" class="theme-btn">Login To Buy Home</a>
